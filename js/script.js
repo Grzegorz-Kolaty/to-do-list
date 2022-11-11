7d7fce70b523443b9f5e28cbc0452b1a316424b8
Grzegorz Kolaty <grzegorzkolaty@gmail.com>
2022-11-11 09:15:32 +0100

    immutability v2

--- js/script.js
+++ js/script.js
@@ -1,14 +1,41 @@
 {
-   let tasks = [];
+   let tasks = [
+      {
+         content: "kodzić troche",
+         done: true,
+      },
+      {
+         content: "pograc w gry",
+         done: false,
+      },
+   ];
+
    let hideDoneTasks = false;
 
-   const addNewTask = (newTaskContent) => {
-      tasks = [
-         ...tasks,
-         { content: newTaskContent },
-      ];
+   const addNewTask = (newTaskContent, taskIndex) => {
+      tasks = tasks.map((task, index) => {
+         if (index === taskIndex) {
+            return {
+               ...task,
+               task: newTaskContent, done: false
+            }
+         }
+      });
+
       render();
    };
+
+
+   /*  kod który działa ale chciałem go zrobić z użyciem .map
+   const addNewTask = (newTaskContent) => {
+        tasks = [
+           ...tasks,
+           { content: newTaskContent, done: false },
+        ];
+        render();
+     };
+  
+  */
 
    const removeTask = (taskIndex) => {
       tasks = [
@@ -16,94 +43,109 @@
          ...tasks.slice(taskIndex + 1),
       ];
       render();
-   }
+   };
 
    const toggleTaskDone = (taskIndex) => {
-      const state = (changestatus) => changestatus = true;
-      tasks[taskIndex] = tasks[taskIndex].map(state)
-
-
-   // tasks[taskIndex].done = !tasks[taskIndex].done;
-   console.log(tasks);
-   console.log(tasks[taskIndex]);
-   render();
-};
-
-const bindEvents = () => {
-   const removeButtons = document.querySelectorAll(".js-remove");
-
-   removeButtons.forEach((removeButtons, index) => {
-      removeButtons.addEventListener("click", () => {
-         removeTask(index);
+      tasks = tasks.map((task, index) => {
+         if (index === taskIndex) {
+            return {
+               ...task,
+               done: !task.done,
+            }
+         }
+         return task;
       });
-   });
 
-   const toggleDoneButtons = document.querySelectorAll(".js-done");
+      render();
+   };
 
-   toggleDoneButtons.forEach((toggleDoneButton, index) => {
-      toggleDoneButton.addEventListener("click", () => {
-         toggleTaskDone(index);
+   /*   kod który działa ale chciałem go zrobić z użyciem .map
+   const toggleTaskDone = (taskIndex) => {
+      tasks = [
+         ...tasks.slice(0, taskIndex),
+         { ...tasks[taskIndex], done: !tasks[taskIndex].done },
+         ...tasks.slice(taskIndex + 1),
+      ];
+      render();
+   };
+   */
+
+
+   const bindEvents = () => {
+      const removeButtons = document.querySelectorAll(".js-remove");
+
+      removeButtons.forEach((removeButtons, index) => {
+         removeButtons.addEventListener("click", () => {
+            removeTask(index);
+         });
       });
-   });
-}
 
-const renderTasks = () => {
-   let htmlString = "";
-   for (const task of tasks) {
-      htmlString += `
+      const toggleDoneButtons = document.querySelectorAll(".js-done");
+
+      toggleDoneButtons.forEach((toggleDoneButton, index) => {
+         toggleDoneButton.addEventListener("click", () => {
+            toggleTaskDone(index);
+         });
+      });
+   }
+
+   const renderTasks = () => {
+      let htmlString = "";
+      for (const task of tasks) {
+         htmlString += `
          <li class="list__tasks">
            <button class="js-done${task.done ? " list__checkButton--true" : " list__checkButton"}">✔</button>
            <span class="list__item${task.done ? " list__item--done" : ""}">${task.content}</span>
            <button class="js-remove list__removeButton">🗑</button>
          </li>
       `;
-   };
+      };
 
-   document.querySelector(".js-tasks").innerHTML = htmlString;
-}
+      document.querySelector(".js-tasks").innerHTML = htmlString;
+   }
 
-const renderButtons = () => { }
+   const renderButtons = () => { }
 
-const bindButtonsEvents = () => {
+   const bindButtonsEvents = () => {
 
-
-}
-
-const render = () => {
-   renderTasks();
-   renderButtons();
-   bindButtonsEvents()
-
-   bindEvents();
-};
-
-const onFormSubmit = (event) => {
-   event.preventDefault();
-
-   const inputField = document.querySelector(".js-newTask");
-   const newTaskContent = inputField.value.trim();
-
-   if (newTaskContent === "") {
-      return;
 
    }
-   addNewTask(newTaskContent);
-   inputField.value = "";  // cleaning up input field 
 
-   const focusField = document.querySelector(".js-newTask--button")
-   focusField.addEventListener("click", (event => {
-      document.querySelector(".js-newTask").focus(); // input field focus after click
-      onFormSubmit(event);
+   const render = () => {
+      renderTasks();
+      renderButtons();
+      bindButtonsEvents()
 
-   }));
-};
+      bindEvents();
+   };
 
-const init = () => {
-   render();
+   const onFormSubmit = (event) => {
+      event.preventDefault();
 
-   const form = document.querySelector(".js-form");
-   form.addEventListener("submit", onFormSubmit);
+      const inputField = document.querySelector(".js-newTask");
+      const newTaskContent = inputField.value.trim();
 
-};
-init();
+      if (newTaskContent === "") {
+         return;
+
+      }
+      addNewTask(newTaskContent);
+      inputField.value = "";  // cleaning up input field 
+
+      const focusField = document.querySelector(".js-newTask--button")
+      focusField.addEventListener("click", (event => {
+         document.querySelector(".js-newTask").focus(); // input field focus after click
+         onFormSubmit(event);
+
+      }));
+   };
+
+   const init = () => {
+      render();
+
+      const form = document.querySelector(".js-form");
+      form.addEventListener("submit", onFormSubmit);
+
+   };
+   init();
 }
